b8702f59ae692acf1b1504e1cf63a8c5d4778a1b
AyeshW <ayeshweerasinghe.16@cse.mrt.ac.lk>
2020-11-07 19:00:54 +0500

    Add routes and CRUD operations

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -14,6 +14,6 @@ const userSchema = new Schema({
     timestamps: true,
 });
 
-const Users = mongoose.model('Users', userSchema);
+const Users = mongoose.model('users', userSchema);
 
 module.exports = Users;
